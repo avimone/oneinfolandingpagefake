2a8cde0b70d2fd0addffe8717c5ef6a1a88b15b9
avimone <40avirajpatel@gmail.com>
2025-09-27 18:13:25 +0530

    experimental logs

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    formats: ['image/webp', 'image/avif'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Skip ESLint during build
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Skip TypeScript errors during build
-  }
+  /* config options here */
 };
 
 export default nextConfig;
--- utilities/appRoutes.ts
+++
@@ -1,7 +0,0 @@
-export const APP_ROUTES = {
-  HOME: "/",
-  SCAN: "/scan",
-  LOGIN: "/login",
-  DASHBOARD: "/dashboard",
-  BOX_SCAN: "/box-scan",
-};
